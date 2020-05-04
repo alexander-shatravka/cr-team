ff2e49691946b41b7a06930d5cee97383a7d5940
Alexander Shatravka <shatravgo2@gmail.com>
2020-05-05 02:46:46 +0300

    new sections

--- js/functions.js
+++ js/functions.js
@@ -62,15 +62,48 @@ function initVideoSliders() {
         arrows: false,
         cssEase: 'cubic-bezier(0.8, 0.1, 0.1, 1)',
     });
+    $('.video-inner-slider-2').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        nextArrow: $('.video-btn-next-2'),
+        prevArrow: $('.video-btn-prev-2'),
+        centerMode: true,
+        dots: false,
+        infinite: true,
+        cssEase: 'cubic-bezier(0.6, 0.2, 0.2, 1)',
+        speed: 2000,
+        draggable: false,
+        asNavFor: '.slider-video-bg-2'
+    });
+    $('.slider-video-bg-2').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        infinite: true,
+        speed: 1000,
+        draggable: false,
+        arrows: false,
+        cssEase: 'cubic-bezier(0.8, 0.1, 0.1, 1)',
+    });
 }
 
 function initVideoSliderButtonPrev() {
-    $('.video-inner-slider').on('beforeChange',function () {
+    $('.video-inner-slider').on('beforeChange', function () {
         $('.video-btn-prev').hide();
         $('.btn-video').hide();
     });
-    $('.video-inner-slider').on('afterChange',function () {
+    $('.video-inner-slider').on('afterChange', function () {
         $('.video-btn-prev').show();
+        $('.btn-video').show();
+    });
+
+    $('.video-inner-slider-2').on('beforeChange', function () {
+        $('.video-btn-prev-2').hide();
+        $('.btn-video').hide();
+    });
+    $('.video-inner-slider-2').on('afterChange', function () {
+        $('.video-btn-prev-2').show();
         $('.btn-video').show();
     });
 }
